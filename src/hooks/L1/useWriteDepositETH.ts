c5fc046ac4f04c3c265007e624be44da07c7f2a8
Matthew Bunday <matthew.bunday@coinbase.com>
2023-11-07 17:16:22 -0500

    Refactor useWriteDepositERC20 and useWriteDepositETH

--- src/hooks/L1/useWriteDepositETH.ts
+++ src/hooks/L1/useWriteDepositETH.ts
@@ -1,60 +1,41 @@
-import { useMutation } from '@tanstack/react-query'
-import { type Config, getPublicClient, getWalletClient, type ResolvedRegister } from '@wagmi/core'
-import {
-  simulateDepositETH,
-  writeDepositETH,
-  type WriteDepositETHParameters as WriteDepositETHActionParameters,
-} from 'op-viem/actions'
-import { useConfig, type UseConfigReturnType } from 'wagmi'
-import type { UseWriteOPActionBaseParameters } from '../../types/UseWriteOPActionBaseParameters.js'
-import type { UseWriteOPActionBaseReturnType } from '../../types/UseWriteOPActionBaseReturnType.js'
+import { optimismPortalABI } from '@eth-optimism/contracts-ts'
+import { type Config } from '@wagmi/core'
+import { type WriteDepositETHParameters as WriteDepositETHActionParameters } from 'op-viem/actions'
+import { useWriteContract, type UseWriteContractParameters } from 'wagmi'
+import { useOpConfig } from '../useOpConfig.js'
 
-export type WriteDepositETHParameters = Omit<WriteDepositETHActionParameters, 'account'> & { chainId?: number }
+export type WriteDepositETHParameters = Omit<WriteDepositETHActionParameters, 'account'> & { l2ChainId: number }
 
 export type UseWriteDepositETHParameters<config extends Config = Config, context = unknown> =
-  UseWriteOPActionBaseParameters<WriteDepositETHParameters, config, context>
-
-export type UseWriteDepositETHReturnType<config extends Config = Config, context = unknown> =
-  & Omit<UseWriteOPActionBaseReturnType<WriteDepositETHParameters, config, context>, 'write' | 'writeAsync'>
-  & {
-    writeDepositETH: UseWriteOPActionBaseReturnType<WriteDepositETHParameters, config, context>['write']
-    writeDepositETHAsync: UseWriteOPActionBaseReturnType<WriteDepositETHParameters, config, context>['writeAsync']
-  }
-
-async function writeMutation(
-  config: UseConfigReturnType,
-  { chainId, ...params }: WriteDepositETHParameters,
-) {
-  const walletClient = await getWalletClient(config, { chainId })
-  const publicClient = getPublicClient(config, { chainId })
-
-  await simulateDepositETH(publicClient, { ...params, account: walletClient.account.address, chain: undefined })
-  return writeDepositETH(walletClient, { ...params, account: walletClient.account.address, chain: undefined })
-}
+  & UseWriteContractParameters<config, context>
+  & WriteDepositETHParameters
 
 /**
  * Deposits ETH to L2 using the OptimismPortal contract
  * @param parameters - {@link UseWriteDepositETHParameters}
  * @returns wagmi [useWriteContract return type](https://alpha.wagmi.sh/react/api/hooks/useWrtieContract#return-type). {@link UseWriteDepositETHReturnType}
  */
-export function useWriteDepositETH<config extends Config = ResolvedRegister['config'], context = unknown>(
-  { mutation: mutationOverride }: UseWriteDepositETHParameters<config, context> = {},
-): UseWriteDepositETHReturnType<config, context> {
-  const config = useConfig()
+export function useWriteDepositETH({ args, l2ChainId }: UseWriteDepositETHParameters) {
+  const config = useOpConfig()
+  const l2Chain = config.l2chains[l2ChainId]
+  const { writeContract, writeContractAsync } = useWriteContract()
 
-  const mutation = {
-    mutationFn(params: WriteDepositETHParameters) {
-      return writeMutation(config, params)
-    },
-    mutationKey: ['writeContract'],
-  }
-
-  const { mutate, mutateAsync, ...result } = useMutation({ ...mutation, ...mutationOverride })
-
-  type Return = UseWriteDepositETHReturnType<config, context>
   return {
-    ...result,
-    writeDepositETH: mutate,
-    writeDepositETHAsync: mutateAsync,
-  } as Return
+    writeDepositETH: () =>
+      writeContract({
+        chainId: l2Chain.l1ChaindId,
+        address: l2Chain.l1Addresses.portal.address,
+        abi: optimismPortalABI,
+        functionName: 'depositTransaction',
+        args: [args.to, args.amount, BigInt(args.gasLimit), false, args.data || '0x'],
+      }),
+    writeDepositETHAsync: () =>
+      writeContractAsync({
+        chainId: l2Chain.l1ChaindId,
+        address: l2Chain.l1Addresses.portal.address,
+        abi: optimismPortalABI,
+        functionName: 'depositTransaction',
+        args: [args.to, args.amount, BigInt(args.gasLimit), false, args.data || '0x'],
+      }),
+  }
 }
